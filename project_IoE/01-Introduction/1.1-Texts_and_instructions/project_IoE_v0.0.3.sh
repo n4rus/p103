e97338e02f1d911ffac36ed2e8ac3ce225485ca9
n4rus <lejsnarus@gmail.com>
2020-08-04 15:12:56 -0300

    update_20200804_project_IoE

---
+++ project_IoE/01-Introduction/1.1-Texts_and_instructions/project_IoE_v0.0.3.sh
@@ -0,0 +1,81 @@
+#!/bin/BASH
+
+'
+#Project Information on Energy
+##############################Presentation
+#The main goal is to develop human ways on using and upgrading its ways
+#on energy and information, wich means understanding and keeping on track
+#the databases and the power grids, as well the impact and range wich
+#every information carries within.
+##############################History_indent
+#While I was developing my initial studies on the matter, Ive made n4rus.com,
+#an web page containing some of the documents I wrote during the PI project,
+#wich was an project that envisioned the launching of free information on the
+#matters contained within the sites content and scope of intent.
+#The aspects that I shall borrow from those, are the matters of improving our
+#ways to generate, distribute, and use energy and information, as well the
+#exploratory needings of our species, researching for the development of
+#interstellar travell, exponentiation of humanity energetic resources in order
+#to maximaze the individual range of reach.
+##############################Object_list
+#With those introductions made, we can list those objects to build:
+#1: Energy generator & container
+#2: Information database
+#3: Computing and confronting Information against Energy
+#4: Spaceship open source models
+#5: Interstellar drive
+#6: Gravity control technology
+#7: Distribution grids new technology
+#8: Isolated life support
+#9: Artificial Ecosystems
+#10: Task Oriented AI
+#11: Human enhancement and protection suit
+#12: Energy & Information peer gathering methods
+#13: Human Knowledge Database & Acessment
+##############################Directories_list
+#The directories in the initial development phase:
+#project_IoE
+#||-PI
+#|-Development
+#|-Object_drafts
+##############################Object_Description
+#Each object draft has a development folder within its respective files
+#Each draft shall have one or more pattent aplied from witch the developers
+#must use as start-line.
+#The goal is to improve humanity as well humans, then any information or
+#relevant technology developed must be shared as open source or freeware,
+#while other version is done within the organization scopes and pattents.
+##############################Version_track
+#Version_0.0.0_Complete_20190410_LEJS
+#|- Document creation
+''
+#Version_0.0.1_Complete_20191510_LEJS
+#|- Object 13 inserted into the list
+#|- Correction on Directories list
+#|- Section naming & version track descriptions
+''
+#Version_0.0.2_Complete_20200323_LEJS
+#|- Creation of p103
+#|- Indentation of last project steps
+#|- Started the renaming task (directories and archives)
+#|- Next step planing: 
+    |- Creation of index.html in the main folder following the README.md file
+    |- Indentation of renamed folders in the html archive
+    |- Start Scanning routine [files/day>version.d_f0-f1]
+''
+#Version_0.0.3_Complete_20200804_LEJS
+#|- Creation of a100e3, a100e3page, a1000e3.github.io
+#|- Re-indentation of last projects and steps
+#|- Restarted the renaming task (directories and archives)
+#|- Created index.html in the a100e3page dir following the p103 guide
+#|- Indented folders in the python and dat archives
+#|- Started Scanning routine [files/day(version_d_f0-fn]
+#|- Next step planing: 
+    |- Upload all files in a folder at project project/IoE/02-folder
+    |- For each file file in n4rus:
+     	      	       transcript      correct		format
+    |- Python	       waiting	       waiting		x
+    |- Dat	       waiting	       waiting		x
+    |- LaTex	       waiting 	       waiting		waiting
+    |- Html	       waiting         waiting  	waiting
+''
